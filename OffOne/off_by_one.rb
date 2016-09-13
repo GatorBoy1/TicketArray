50d82bcca0245e21dadd525b8eadf842910d0835
Don Kingery <kingery1@suddenlink.net>
2016-09-13 13:56:21 -0400

    5thClass_OneOff_ArrayTest

---
+++ OffOne/off_by_one.rb
@@ -0,0 +1,19 @@
+def off_by_one?(ticket_1, ticket_2)
+	dont_match = 0
+	i = 0
+
+	4.times do
+		if ticket_1[i] != ticket_2[i]
+		dont_match += 1
+	end
+	i += 1
+end
+
+	dont_match == 1
+end
+
+def find_tickets_one_off(my_ticket, winning_tickets)
+	find_tickets_one_off = []
+end
+
+				
